3d329072db50c233695c1f75ca051f7bef3597b7
pyrex41 <reub.brooks@gmail.com>
2026-09-04 09:31:41 -0500

    Update Nix flake platform support

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "shen-erl development environment";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { nixpkgs, ... }: let systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ]; each = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system}); in {
+  outputs = { nixpkgs, ... }: let systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ]; each = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system}); in {
     packages = each (pkgs: { toolchain = pkgs.buildEnv { name = "shen-erl-toolchain"; paths = with pkgs; [ gnumake gcc beamPackages.erlang ]; }; default = pkgs.buildEnv { name = "shen-erl-toolchain"; paths = with pkgs; [ gnumake gcc beamPackages.erlang ]; }; });
     devShells = each (pkgs: { default = pkgs.mkShell { packages = with pkgs; [ gnumake gcc beamPackages.erlang ]; }; });
   };
